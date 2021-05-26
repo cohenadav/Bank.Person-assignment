33d670143b46770619170b14042a323a98d6f341
cohenadav <cohenadav@gmail.com>
2021-05-26 15:24:49 +0300

    added lombok anotation to person and address

--- bank-microservice/src/main/java/nadavc/assignment/bankmicroservice/models/Person.java
+++ bank-microservice/src/main/java/nadavc/assignment/bankmicroservice/models/Person.java
@@ -1,6 +1,14 @@
 package nadavc.assignment.bankmicroservice.models;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Person {
 
     private int id;
@@ -11,86 +19,6 @@ public class Person {
     private int weightInKg;
     private Address address;
 
-
-
-    public Person() {
-
-    }
-
-    /*
-    public Person(Person other) {
-        this.id = other.id;
-        this.name = other.name;
-        this.age = other.age;
-        this.gender = other.gender;
-        this.heightInCm = other.heightInCm;
-        this.weightInKg = other.weightInKg;
-        this.address = other.address;
-    }
-*/
-    public Person(int id, String name, int age, Gender gender, int heightInCm, int weightInKg, Address address) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.heightInCm = heightInCm;
-        this.weightInKg = weightInKg;
-        this.address = address;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public int getHeightInCm() {
-        return heightInCm;
-    }
-
-    public void setHeightInCm(int heightInCm) {
-        this.heightInCm = heightInCm;
-    }
-
-    public int getWeightInKg() {
-        return weightInKg;
-    }
-
-    public void setWeightInKg(int weightInKg) {
-        this.weightInKg = weightInKg;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 }
+
+
--- bank-microservice/src/test/java/nadavc/assignment/bankmicroservice/BankMicroserviceApplicationTests.java
+++ bank-microservice/src/test/java/nadavc/assignment/bankmicroservice/BankMicroserviceApplicationTests.java
@@ -1,10 +1,19 @@
 package nadavc.assignment.bankmicroservice;
 
+import nadavc.assignment.bankmicroservice.services.BankService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BankMicroserviceApplicationTests {
+
+	@Autowired
+	private BankService bankService;
+
+	//@MockBean
+	//private Person
 
 	@Test
 	void contextLoads() {
