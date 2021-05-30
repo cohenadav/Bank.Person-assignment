06f818decaea3ab73d158e6b631ae9f987038137
cohenadav <cohenadav@gmail.com>
2021-05-30 13:26:10 +0300

    change controller routing to Bank MS

--- bank-microservice/src/main/java/nadavc/assignment/bankmicroservice/resources/BankController.java
+++ bank-microservice/src/main/java/nadavc/assignment/bankmicroservice/resources/BankController.java
@@ -4,6 +4,7 @@ import nadavc.assignment.bankmicroservice.models.Person;
 import nadavc.assignment.bankmicroservice.models.PersonsDetails;
 import nadavc.assignment.bankmicroservice.services.BankService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,27 +13,28 @@ import java.net.URISyntaxException;
 
 
 @RestController
+@RequestMapping("/api/costumers/")
 public class BankController {
 
     @Autowired
     private BankService bankService;
 
-    @RequestMapping("/costumers/{id}")
+    @GetMapping("{id}")
     public Person getCostumerDetails(@PathVariable String id) throws URISyntaxException {
         return bankService.getCostumerDetails(id);
     }
 
-    @RequestMapping("/costumers")
+    @GetMapping()
     public PersonsDetails getCostumerDetails() throws URISyntaxException {
         return bankService.getAllCostumer();
     }
 
-    @RequestMapping("/costumers/israel")
+    @GetMapping("israel")
     public PersonsDetails getCostumersFromIsrael() throws URISyntaxException {
         return bankService.getCostumersFromIsrael();
     }
 
-    @RequestMapping("/costumers/ak")
+    @GetMapping("ak")
     public PersonsDetails getCostumersStartsWithAOrK() throws URISyntaxException {
         return bankService.getCostumersStartsWithAOrK();
     }
