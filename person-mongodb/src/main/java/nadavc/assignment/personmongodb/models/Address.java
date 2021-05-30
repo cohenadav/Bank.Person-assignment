136d40fd9bf6fecaa79f42b672f0698c227800cc
cohenadav <cohenadav@gmail.com>
2021-05-30 12:37:47 +0300

    Add Allargsconst to Person and Address models

--- person-mongodb/src/main/java/nadavc/assignment/personmongodb/models/Address.java
+++ person-mongodb/src/main/java/nadavc/assignment/personmongodb/models/Address.java
@@ -1,10 +1,12 @@
 package nadavc.assignment.personmongodb.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class Address {
--- person-mongodb/src/main/java/nadavc/assignment/personmongodb/models/Person.java
+++ person-mongodb/src/main/java/nadavc/assignment/personmongodb/models/Person.java
@@ -1,15 +1,13 @@
 package nadavc.assignment.personmongodb.models;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Document
